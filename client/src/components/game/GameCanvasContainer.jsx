ae04d06f3573ed7d61879d1114b0ca5439264d0d
Thomas Brockington <xtombrock1989@gmail.com>
2023-05-10 23:13:55 +0100

    basic canvas

--- client/src/components/game/GameCanvasContainer.jsx
+++ client/src/components/game/GameCanvasContainer.jsx
@@ -1,7 +1,66 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function GameCanvasContainer() {
-  return <div>GameCanvasContainer</div>;
+  const useScript = (url) => {
+    useEffect(() => {
+        
+      let canvasBgColour = `#00f000`;
+
+      const canvas = document.createElement('canvas');
+      const context = canvas.getContext('2d');
+      const container = document.querySelector('#game-container');
+
+      container.appendChild(canvas);
+
+      var rect = canvas.parentNode.getBoundingClientRect();
+      canvas.width = rect.width;
+      canvas.height = rect.height;
+
+      // Draw canvas
+      context.fillStyle = canvasBgColour;
+      context.fillRect(0, 0, canvas.width, canvas.height);
+
+      // Mouse size
+      let mouse = {
+        x: null,
+        y: null,
+        radius: (canvas.height / 90) * (canvas.width / 90),
+      };
+
+      // Mouse position
+      container.addEventListener('mousemove', function (e) {
+        mouse.x = e.x;
+        mouse.y = e.y;
+      });
+
+      // Listen for clicks
+      // If click hits an enemy. They run the 'clicked()' function
+      canvas.addEventListener('click', (e) => {
+        var x = e.pageX - canvas.offsetLeft;
+        var y = e.pageY - canvas.offsetTop;
+      });
+
+      // Redraw each frame of the screen
+    //   function updateSwarm() {
+    //     requestAnimationFrame(updateSwarm);
+    //     context.clearRect(0, 0, canvas.width, canvas.height);
+    //     enemySwarmArray.forEach((algae) => {
+    //       algae.update();
+    //     });
+    //   }
+
+      function run() {
+      }
+
+      run();
+    }, [url]);
+  };
+
+  return (
+    <div id='game-container' className='grid absolute w-full h-full'>
+      {useScript()}
+    </div>
+  );
 }
 
 export default GameCanvasContainer;
