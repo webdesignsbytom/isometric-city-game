d49d2ac3bd66996acf9b6cc294ccd3f0cd8abdd3
xtombrock <xtombrock1989@gmail.com>
2023-05-11 02:23:38 +0100

    grid working

--- client/src/components/game/GameCanvasContainer.jsx
+++ client/src/components/game/GameCanvasContainer.jsx
@@ -1,17 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function GameCanvasContainer() {
-  const useScript = (url) => {
+  // Starting Title Size
+  const [startingTileNumber, setStartingTileNumber] = useState({
+    y: 10,
+    x: 10,
+  });
+  const [tileSize, setTileSize] = useState({
+    y: 20,
+    x: 20,
+  });
+  const [startingTilePosition, setStartingTilePosition] = useState({
+    xpos: 10,
+    ypos: 10,
+  });
+  const [totalUnlockedTiles, setTotalUnlockedTiles] = useState(0);
+  const [tileIdNumber, setTileIdNumber] = useState(1);
+
+  console.log('totalUnlockedTiles', totalUnlockedTiles);
+  console.log('startingTilePosition', startingTilePosition);
+
+  const useScript = () => {
     useEffect(() => {
-        
       let canvasBgColour = `#00f000`;
 
       const canvas = document.createElement('canvas');
       const context = canvas.getContext('2d');
       const container = document.querySelector('#game-container');
-
+      // canvas.style = "position: absolute; top: 0px; left: 0px; right: 0px; bottom: 0px; margin: auto; border:2px solid blue";
       container.appendChild(canvas);
 
+      // Set size of canvas based on it parent
       var rect = canvas.parentNode.getBoundingClientRect();
       canvas.width = rect.width;
       canvas.height = rect.height;
@@ -34,26 +53,71 @@ function GameCanvasContainer() {
       });
 
       // Listen for clicks
-      // If click hits an enemy. They run the 'clicked()' function
       canvas.addEventListener('click', (e) => {
         var x = e.pageX - canvas.offsetLeft;
         var y = e.pageY - canvas.offsetTop;
       });
 
-      // Redraw each frame of the screen
-    //   function updateSwarm() {
-    //     requestAnimationFrame(updateSwarm);
-    //     context.clearRect(0, 0, canvas.width, canvas.height);
-    //     enemySwarmArray.forEach((algae) => {
-    //       algae.update();
-    //     });
-    //   }
+      class Tile {
+        constructor(id, xpos, ypos, xsize, ysize) {
+          this.id = id;
+          this.xpos = xpos;
+          this.ypos = ypos;
+          this.xsize = xsize;
+          this.ysize = ysize;
+        }
 
-      function run() {
+        draw(context) {
+          context.beginPath();
+          context.strokeRect(this.xpos, this.ypos, this.xsize, this.ysize);
+          context.stroke();
+        }
       }
 
-      run();
-    }, [url]);
+      function createTileArray(context) {
+        console.log('createTileArray');
+        let dx = 0;
+        let dy = 0;
+        let id = tileIdNumber;
+
+        for (var y = 0; y < startingTileNumber.y; y++) {
+          for (var x = 0; x < startingTileNumber.x; x++) {
+            const newTile = new Tile(id, dx, dy, tileSize.x, tileSize.y);
+            dy += 20;
+            id++;
+            newTile.draw(context);
+          }
+          id++;
+          dy = 0;
+          dx += 20;
+        }
+      }
+
+      function run(context) {
+        createTileArray(context);
+      }
+
+      run(context);
+
+      // function renderMap(context) {
+      //   var dx = 0,
+      //     dy = 0;
+      //   context.save();
+
+      //   context.scale(1, 0.5);
+      //   context.rotate((45 * Math.PI) / 180);
+
+      //   for (var y = 0; y < startingTileNumber.y; y++) {
+      //     for (var x = 0; x < startingTileNumber.x; x++) {
+      //       context.strokeRect(dx, dy, 40, 40);
+      //       dx += 40;
+      //     }
+      //     dx = 0;
+      //     dy += 40;
+      //   }
+      // }
+      // renderMap(context);
+    }, []);
   };
 
   return (
--- client/src/pages/welcome/WelcomePage.jsx
+++ client/src/pages/welcome/WelcomePage.jsx
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import GameCanvasContainer from '../../components/game/GameCanvasContainer';
 
 function WelcomePage() {
+  const [startedGame, setStartedGame] = useState(false);
+
   const gameStart = () => {
     console.log('Starting game');
+    setStartedGame(true);
   };
 
   return (
-    <div  className='grid h-screen justify-center items-center'>
-      <section className='outline outline-2 outline-black py-8 px-6 rounded-xl bg-stone-200'>
-        <article className='grid items-center justify-center text-center gap-4'>
-          <h1>Welcome To IsoCity</h1>
-          <h2>Click To Play Now</h2>
-          <div>
-            <button className='outline outline-2 outline-black p-2 bg-slate-400 rounded' onClick={gameStart}>Start Game!</button>
+    <div className='grid'>
+      {startedGame ? (
+        <section className='grid h-screen p-4'>
+          <div className='grid relative'>
+            <GameCanvasContainer />
           </div>
-        </article>
-      </section>
+        </section>
+      ) : (
+        <section className='grid h-screen p-4 bg-stone-200'>
+          <div className='grid items-center justify-center outline outline-2 outline-black py-8 px-6 rounded-xl bg-red-200'>
+            <article className='grid text-center gap-4 outline outline-2 outline-black py-8 px-6 rounded-xl bg-red-400'>
+              <h1>Welcome To IsoCity</h1>
+              <h2>Click To Play Now</h2>
+              <div>
+                <button
+                  className='outline outline-2 outline-black p-2 bg-slate-400 rounded'
+                  onClick={gameStart}
+                >
+                  Start Game!
+                </button>
+              </div>
+            </article>
+          </div>
+        </section>
+      )}
     </div>
   );
 }
